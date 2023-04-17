3b55fa3fc5278077ccad50dc3d9bc4c12af4fe9c
David Connaughton <15805713+eolasd@users.noreply.github.com>
2023-04-17 10:16:15 +1000

    Create WebSearchOptions.cs

---
+++ WebSearchOptions.cs
@@ -0,0 +1,8 @@
+namespace SkTestApp.Configuration
+{
+    public class WebSearchOptions
+    {
+        public string GoogleSearchApiKey { get; set; }
+        public string GoogleSearchCseId { get; set; }
+    }
+}
